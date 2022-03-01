4d1d9049ea7bbdce684e59cdbbd2283cf119ec60
unofficialdxnny <82535503+unofficialdxnny@users.noreply.github.com>
2022-03-01 23:42:18 +0000

    Add files via upload

--- function.js
+++ function.js
@@ -44,3 +44,18 @@
  });  
  
   })(jQuery);
+
+  const panels = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+ this.classList.toggle('open');
+}
+
+function toggleActive(e) {
+ if(e.propertyName.includes('flex')) {
+   this.classList.toggle('open-active');
+  }
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
