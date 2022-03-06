f4561f493c31f8869311a7b412d5379e09fc0aae
unofficialdxnny <82535503+unofficialdxnny@users.noreply.github.com>
2022-03-06 20:08:22 +0000

    Add files via upload

--- function.js
+++ function.js
@@ -64,3 +64,41 @@ panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
 
 
 
+const countEl = document.getElementById('count');
+
+updateVisitCount();
+
+function updateVisitCount() {
+	fetch('https://api.countapi.xyz/update/unofficialdxnny.netlify.app/website/?amount=1')
+	.then(res => res.json())
+	.then(res => {
+		countEl.innerHTML = res.value;
+	})
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+	social_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', () => {
+	social_panel_container.classList.remove('visible')
+});
+
+
