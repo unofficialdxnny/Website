70f8ab34f9e291e345f07c5893a8e8fb59fc0e7c
unofficialdxnny <82535503+unofficialdxnny@users.noreply.github.com>
2022-03-07 18:51:41 +0000

    Add files via upload

--- website/function.js
+++ website/function.js
@@ -102,3 +102,22 @@ close_btn.addEventListener('click', () => {
 });
 
 
+// Scroll to specific values
+// scrollTo is the same
+window.scroll({
+	top: 2500, 
+	left: 0, 
+	behavior: 'smooth'
+  });
+  
+  // Scroll certain amounts from current position 
+  window.scrollBy({ 
+	top: 100, // could be negative value
+	left: 0, 
+	behavior: 'smooth' 
+  });
+  
+  // Scroll to a certain element
+  document.querySelector('.hello').scrollIntoView({ 
+	behavior: 'smooth' 
+  });
