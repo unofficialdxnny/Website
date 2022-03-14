96d477474bae53eb75fb07a9a66aa3f26799d846
unofficialdxnny <82535503+unofficialdxnny@users.noreply.github.com>
2022-03-14 20:39:18 +0000

    Add files via upload

--- website/function.js
+++ website/function.js
@@ -129,4 +129,17 @@ img.ondragstart = () => {
   return false;
 };
 
-//change text of <p> on hover
+
+
+// greetings
+
+const greeting = document.getElementById("greeting");
+const hour = new Date().getHours();
+const welcomeTypes = ["Good morning", "Good afternoon", "Good evening"];
+let welcomeText = "";
+
+if (hour < 12) welcomeText = welcomeTypes[0];
+else if (hour < 18) welcomeText = welcomeTypes[1];
+else welcomeText = welcomeTypes[2];
+
+greeting.innerHTML = welcomeText;
