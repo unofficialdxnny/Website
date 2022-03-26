29ed8521f33800cf0b81fda61193d2e373fd7822
unofficialdxnny <82535503+unofficialdxnny@users.noreply.github.com>
2022-03-26 22:07:33 +0000

    cleaned up the directory

---
+++ website/main.js
@@ -0,0 +1,145 @@
+(function($) { "use strict";
+ 
+ $(function() {
+ var header = $(".start-style");
+ $(window).scroll(function() {    
+ var scroll = $(window).scrollTop();
+ 
+ if (scroll >= 10) {
+ header.removeClass('start-style').addClass("scroll-on");
+ } else {
+ header.removeClass("scroll-on").addClass('start-style');
+ }
+ });
+ }); 
+ 
+ //Animation
+ 
+ $(document).ready(function() {
+ $('body.hero-anime').removeClass('hero-anime');
+ });
+ 
+ //Menu On Hover
+ 
+ $('body').on('mouseenter mouseleave','.nav-item',function(e){
+ if ($(window).width() > 750) {
+ var _d=$(e.target).closest('.nav-item');_d.addClass('show');
+ setTimeout(function(){
+ _d[_d.is(':hover')?'addClass':'removeClass']('show');
+ },1);
+ }
+ }); 
+ 
+ //Switch light/dark
+ 
+ $("#switch").on('click', function () {
+ if ($("body").hasClass("dark")) {
+ $("body").removeClass("dark");
+ $("#switch").removeClass("switched");
+ }
+ else {
+ $("body").addClass("dark");
+ $("#switch").addClass("switched");
+ }
+ });  
+ 
+  })(jQuery);
+
+  const panels = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+ this.classList.toggle('open');
+}
+
+function toggleActive(e) {
+ if(e.propertyName.includes('flex')) {
+   this.classList.toggle('open-active');
+  }
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+
+
+
+
+
+const countEl = document.getElementById('count');
+
+updateVisitCount();
+
+function updateVisitCount() {
+	fetch('https://api.countapi.xyz/update/unofficialdxnny.netlify.app/website/?amount=1')
+	.then(res => res.json())
+	.then(res => {
+		countEl.innerHTML = res.value;
+	})
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+	social_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', () => {
+	social_panel_container.classList.remove('visible')
+});
+
+
+// Scroll to specific values
+// scrollTo is the same
+window.scroll({
+	top: 2500, 
+	left: 0, 
+	behavior: 'smooth'
+  });
+  
+  // Scroll certain amounts from current position 
+  window.scrollBy({ 
+	top: 100, // could be negative value
+	left: 0, 
+	behavior: 'smooth' 
+  });
+  
+  // Scroll to a certain element
+  document.querySelector('.hello').scrollIntoView({ 
+	behavior: 'smooth' 
+  });
+
+
+  // disable the image drag
+  const img = document.querySelector('img')
+img.ondragstart = () => {
+  return false;
+};
+
+
+
+// greetings
+
+const greeting = document.getElementById("greeting");
+const hour = new Date().getHours();
+const welcomeTypes = ["Good morning", "Good afternoon", "Good evening"];
+let welcomeText = "";
+
+if (hour < 12) welcomeText = welcomeTypes[0];
+else if (hour < 18) welcomeText = welcomeTypes[1];
+else welcomeText = welcomeTypes[2];
+
+greeting.innerHTML = welcomeText;
