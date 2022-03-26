23c4ec7af129a8ed6368b96884d241e13168725b
unofficialdxnny <82535503+unofficialdxnny@users.noreply.github.com>
2022-03-26 22:56:55 +0000

    added projects page

---
+++ website/navbar.js
@@ -0,0 +1,90 @@
+(function($) { "use strict";
+ 
+ $(function() {
+ var header = $(".start-style");
+ $(window).scroll(function() {    
+ var scroll = $(window).scrollTop();
+ 
+ if (scroll >= 10) {
+ header.removeClass('start-style').addClass("scroll-on");
+ } else {
+ header.removeClass("scroll-on").addClass('start-style');
+ }
+ });
+ }); 
+ 
+ //Animation
+ 
+ $(document).ready(function() {
+ $('body.hero-anime').removeClass('hero-anime');
+ });
+ 
+ //Menu On Hover
+ 
+ $('body').on('mouseenter mouseleave','.nav-item',function(e){
+ if ($(window).width() > 750) {
+ var _d=$(e.target).closest('.nav-item');_d.addClass('show');
+ setTimeout(function(){
+ _d[_d.is(':hover')?'addClass':'removeClass']('show');
+ },1);
+ }
+ }); 
+ 
+ //Switch light/dark
+ 
+ $("#switch").on('click', function () {
+ if ($("body").hasClass("dark")) {
+ $("body").removeClass("dark");
+ $("#switch").removeClass("switched");
+ }
+ else {
+ $("body").addClass("dark");
+ $("#switch").addClass("switched");
+ }
+ });  
+ 
+  })(jQuery);
+
+  const panels = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+ this.classList.toggle('open');
+}
+
+function toggleActive(e) {
+ if(e.propertyName.includes('flex')) {
+   this.classList.toggle('open-active');
+  }
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+
+
+
+
+
+
+// Scroll to specific values
+// scrollTo is the same
+window.scroll({
+	top: 2500, 
+	left: 0, 
+	behavior: 'smooth'
+  });
+  
+  // Scroll certain amounts from current position 
+  window.scrollBy({ 
+	top: 100, // could be negative value
+	left: 0, 
+	behavior: 'smooth' 
+  });
+  
+  // Scroll to a certain element
+  document.querySelector('.hello').scrollIntoView({ 
+	behavior: 'smooth' 
+  });
+
+
+
+
