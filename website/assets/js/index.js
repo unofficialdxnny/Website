c3973466e7754446f400e9c29ead984e85d140cb
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2022-06-08 06:21:54 +0100

    added color to console

--- website/assets/js/index.js
+++ website/assets/js/index.js
@@ -1,7 +1,38 @@
+function colorate(message, color) {
+
+  color = color || "black";
+
+  switch (color) {
+      case "success":  
+           color = "Green"; 
+           break;
+      case "info":     
+              color = "DodgerBlue";  
+           break;
+      case "error":   
+           color = "Red";     
+           break;
+      case "warning":  
+           color = "Orange";   
+           break;
+      default: 
+           color = color;
+  }
+
+  console.log("%c" + message, "color:" + color);
+}
+
+colorate('unofficialdxnny Website - Remaking since 07/06/2022 04:00', "info")
 // updates / changelog for my site :)
 fetch('/assets/update.txt')
   .then(response => response.text())
   .then(data => {
   	
-  	console.log(data);
+  	colorate(data, "info")
   });
+
+const custom = {
+     command() {
+        console.log("This is my custom command.");
+    },
+}; 
