41bf8a822df3dbac1ba17e92bc691deab6b5e4d6
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2022-12-24 18:47:24 +0000

    new looks

--- updat.js
+++
@@ -1,4 +0,0 @@
-setInterval(function() {
-  var myImageElement = document.getElementById('Spotify-now-playing');
-  myImageElement.src = 'https://spotify-github-profile.vercel.app/api/view.svg?uid=ahmeddanial&cover_image=true&theme=novatorem&show_offline=true&background_color=121212&bar_color=f2aa4c&bar_color_cover=false?rand=' + Math.random();
-}, 3000);
