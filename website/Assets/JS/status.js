88d49e0db1c6872374da70ecdc24ef1e089bfbeb
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2022-12-25 18:01:45 +0000

    upadate

--- website/Assets/JS/status.js
+++ website/Assets/JS/status.js
@@ -1,9 +1,10 @@
-async function fetchDataAsync(url) {
-    const response = await fetch(url);
-    console.log(await response.json());
-    
-
-}
-
-fetchDataAsync('https://api.lanyard.rest/v1/users/898937224895270972');
-
+// async function fetchDataAsync(url) {
+//     const response = await fetch(url);
+//     console.log(await response.json());
+//     
+// 
+// }
+// 
+// fetchDataAsync('https://api.lanyard.rest/v1/users/898937224895270972');
+// 
+// 
