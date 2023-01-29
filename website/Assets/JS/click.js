4b303e1cdb7d99503e10e5367b4b7d061602dcff
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-01-29 21:52:10 +0000

    fixed theme toggle
    Now i have done this you will no longer be getting an epileptic seizures.

--- website/Assets/JS/click.js
+++
@@ -1,7 +0,0 @@
-// function MouseSound() {
-//     
-//     var audio = new Audio('./Assets/Sound/click.wav');
-//     audio.play();
-// }
-// 
-// window.addEventListener('click', MouseSound , false);
--- website/Assets/JS/darkmode.js
+++ website/Assets/JS/darkmode.js
@@ -1,10 +1,29 @@
+document.addEventListener("keyup", e => {
+   if (e.key.toLowerCase() == 't') {
+      var element = document.body;
+      element.classList.toggle("dark-mode");
+      
 
-if (localStorage.setItem("theme", "light")) {
    
-   document.body.style.backgroundColor = "Alto";
+   }
 
-} 
 
-if (localStorage.setItem("theme", "dark")) {
-   document.body.style.backgroundColor = "Mine Shaft"
-}
+});
+
+// 
+// if (localStorage.getItem("theme", "null")) {
+//    document.body.style.backgroundColor = "Mine Shaft"
+// }
+// 
+// if (localStorage.setItem("theme", "light")) {
+//    
+//    document.body.style.backgroundColor = "Alto";
+// 
+// } 
+// 
+// if (localStorage.getItem("theme", "dark")) {
+//    document.body.style.backgroundColor = "Mine Shaft"
+// }
+// 
+
+
