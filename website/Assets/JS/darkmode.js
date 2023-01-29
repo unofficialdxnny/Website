a02f6a1222b8c3f9365e3b907f07639db4e19d42
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-01-29 22:19:31 +0000

    fighting js...

--- website/Assets/JS/darkmode.js
+++ website/Assets/JS/darkmode.js
@@ -1,29 +1,40 @@
-document.addEventListener("keyup", e => {
-   if (e.key.toLowerCase() == 't') {
-      var element = document.body;
-      element.classList.toggle("dark-mode");
-      
 
+
+if (localStorage.getItem("theme") == null) {
    
+   localStorage.setItem("theme", "light")
+   
+   document.addEventListener("keyup", e => {
+      
+      if (e.key.toLowerCase() == 't') {
+         
+         var element = document.body;
+         
+         element.classList.toggle("dark-mode"); 
+
+         document.body.style.backgroundColor = "Mine Shaft"
+      
+      
+      } else if (localStorage.getItem("theme") == 'light') {
+
+         if (e.key.toLowerCase() == 't') {
+            
+            localStorage.setItem("theme", "dark")
+
+            var element = document.body;
+
+            element.classList.toggle("dark-mode");
+            
+
+            document.body.style.backgroundColor = "Alto";
+
+      }
    }
+   
+   
+   });
 
 
-});
-
-// 
-// if (localStorage.getItem("theme", "null")) {
-//    document.body.style.backgroundColor = "Mine Shaft"
-// }
-// 
-// if (localStorage.setItem("theme", "light")) {
-//    
-//    document.body.style.backgroundColor = "Alto";
-// 
-// } 
-// 
-// if (localStorage.getItem("theme", "dark")) {
-//    document.body.style.backgroundColor = "Mine Shaft"
-// }
-// 
+}
 
 
