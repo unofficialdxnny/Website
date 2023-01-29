6f3381eeb8e339f0e7e4b1749734ced656c58078
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-01-29 23:05:37 +0000

    it works improperly...
    the localstorage is being weird

--- website/Assets/JS/darkmode.js
+++ website/Assets/JS/darkmode.js
@@ -1,40 +1,54 @@
 
+// If localstorage theme key is empty or nonexistent than change theme to light mode
 
 if (localStorage.getItem("theme") == null) {
    
    localStorage.setItem("theme", "light")
+
    
-   document.addEventListener("keyup", e => {
-      
-      if (e.key.toLowerCase() == 't') {
-         
-         var element = document.body;
-         
-         element.classList.toggle("dark-mode"); 
-
-         document.body.style.backgroundColor = "Mine Shaft"
-      
-      
-      } else if (localStorage.getItem("theme") == 'light') {
-
-         if (e.key.toLowerCase() == 't') {
-            
-            localStorage.setItem("theme", "dark")
-
-            var element = document.body;
-
-            element.classList.toggle("dark-mode");
-            
-
-            document.body.style.backgroundColor = "Alto";
-
-      }
-   }
-   
-   
-   });
-
 
 }
+
+     
+   if (localStorage.getItem("theme") == "light") {
+      document.body.style.backgroundColor = "Mine Shaft"
+
+      document.addEventListener("keyup", e => {
+         if (e.key.toLowerCase() == 't') {
+            localStorage.clear()
+            localStorage.setItem("theme", "dark")
+            var element = document.body;
+            element.classList.toggle("dark-mode");
+            
+            document.body.style.backgroundColor = "Alto";
+   
+        
+            
+            } 
+         });
+
+      } else if (localStorage.getItem("theme") == "dark") {
+         
+
+         document.body.style.backgroundColor = "Alto";
+         
+         document.addEventListener("keyup", e => {
+            if (e.key.toLowerCase() == 't') {
+               localStorage.clear()
+               localStorage.setItem("theme", "light")
+               var element = document.body;
+               element.classList.toggle("dark-mode");
+               
+               document.body.style.backgroundColor = "Mine Shaft"
+      
+           
+               
+               } 
+            });
+
+
+
+      } 
+
 
 
