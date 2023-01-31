0585c88819d883803b887ab98f1a8224805c2634
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-01-31 12:44:52 +0000

    i acc did it this time (i think)
    Localstorage setup

--- website/Assets/JS/darkmode.js
+++ website/Assets/JS/darkmode.js
@@ -1,57 +1,41 @@
-
 //  If localstorage theme key is empty or nonexistent than change theme to light mode
  
- if (localStorage.getItem("theme") == null) {
+if (localStorage.getItem("theme") == null) {
     
-    localStorage.setItem("theme", "light")
- 
-    
- 
+   localStorage.setItem("theme", "light")
+   document.body.style.backgroundColor = "Mine Shaft"
+
+ } else if (localStorage.getItem("theme") == "dark") {
+
+   document.body.style.backgroundColor = "Alto"
+ } else if (localStorage.getItem("theme") == "light") {
+
+   document.body.style.backgroundColor = "Mine Shaft"
+
  }
  
+ 
+ 
+ 
+ if (document.addEventListener("keyup", e => {
+   if (e.key.toLowerCase() == 't') {
       
-    if (localStorage.getItem("theme") == "light") {
-       document.body.style.backgroundColor = "Mine Shaft"
- 
-       document.addEventListener("keyup", e => {
-          if (e.key.toLowerCase() == 't') {
-             localStorage.clear()
-             localStorage.setItem("theme", "dark")
-             var element = document.body;
-             element.classList.toggle("dark-mode");
-             
-             document.body.style.backgroundColor = "Alto";
-    
-         
-             
-             } 
-          });
- 
-       } else if (localStorage.getItem("theme") == "dark") {
-          
- 
-          document.body.style.backgroundColor = "Alto";
-          
-          document.addEventListener("keyup", e => {
-             if (e.key.toLowerCase() == 't') {
-                localStorage.clear()
-                localStorage.setItem("theme", "light")
-                var element = document.body;
-                element.classList.toggle("dark-mode");
-                
-                document.body.style.backgroundColor = "Mine Shaft"
-       
-            
-                
-                } 
-             });
- 
- 
- 
-       } 
- 
- 
- 
- 
+      if (localStorage.getItem("theme") == "light") {
+         localStorage.clear()
+         localStorage.setItem("theme", "dark")
+         var element = document.body;
+         element.classList.toggle("dark-mode");
 
+      } else if (localStorage.getItem("theme") == "dark") {
+         localStorage.clear()
+         localStorage.setItem("theme", "light")
+         var element = document.body;
+         element.classList.toggle("dark-mode");
+
+      }
+      
+      
+          
+          }
+         }));
 
