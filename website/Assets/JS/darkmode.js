a3af5c2e16160d876c42d9043a8e99a945aab79a
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-02-02 00:59:29 +0000

    manually toggle between themes

--- website/Assets/JS/darkmode.js
+++ website/Assets/JS/darkmode.js
@@ -1,2 +1,13 @@
+document.addEventListener("keyup", e => {
+      
+    if (e.key.toLowerCase() == 't') {
+       
+       var element = document.body;
+       
+       element.classList.toggle("dark-mode"); 
 
 
+    
+    
+    }
+})
