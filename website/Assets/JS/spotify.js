55727648788a220a6b5ee78121a576111d928189
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-03-07 23:26:36 +0000

    added spotify now playing

--- website/Assets/JS/spotify.js
+++ website/Assets/JS/spotify.js
@@ -1,4 +1,4 @@
 setInterval(function() {
     var myImageElement = document.getElementById('Spotify-now-playing');
-    myImageElement.src = 'https://spotify-github-profile.vercel.app/api/view?uid=ahmeddanial&cover_image=false&theme=novatorem&show_offline=false&background_color=121212&interchange=false&bar_color=c3cbd4&bar_color_cover=fals' + Math.random();
+    myImageElement.src = 'https://spotify-github-profile.vercel.app/api/view?uid=ahmeddanial&cover_image=true&theme=novatorem&show_offline=false&background_color=121212&interchange=false&bar_color=53b14f&bar_color_cover=false' + Math.random();
   }, 3000);
