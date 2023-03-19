59087c9236e8b5fbe6fb8de9ba3aacec01a553b5
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-03-19 17:15:12 +0000

    added tap to change theme too

--- website/Assets/JS/theme.js
+++ website/Assets/JS/theme.js
@@ -1,5 +1,4 @@
-// Get the theme toggle button and body element
-const themeToggle = document.getElementById("theme-toggle");
+// Get the body element
 const body = document.body;
 
 // Check if a theme is already set in local storage and apply it
@@ -8,12 +7,37 @@ if (currentTheme) {
   body.classList.add(currentTheme);
 }
 
-// Add a click event listener to the theme toggle button
-themeToggle.addEventListener("click", () => {
-  // Toggle the "dark" class on the body element
-  body.classList.toggle("dark");
-  
-  // Save the current theme choice to local storage
-  const theme = body.classList.contains("dark") ? "dark" : "light";
-  localStorage.setItem("theme", theme);
+// Add a keyup event listener to the document object
+document.addEventListener("keyup", (event) => {
+  // Check if the "t" key was released
+  if (event.key === "t") {
+    // Toggle the "dark" class on the body element
+    body.classList.toggle("dark");
+
+    // Save the current theme choice to local storage
+    const theme = body.classList.contains("dark") ? "dark" : "light";
+    localStorage.setItem("theme", theme);
+  }
+});
+
+// Initialize a variable to store the timestamp of the last tap
+let lastTap = 0;
+
+// Add a touchend event listener to the document object
+document.addEventListener("touchend", (event) => {
+  // Get the current timestamp
+  const now = new Date().getTime();
+
+  // Check if this is a double tap (i.e. the second tap occurred within 500ms of the first tap)
+  if (now - lastTap < 500) {
+    // Toggle the "dark" class on the body element
+    body.classList.toggle("dark");
+
+    // Save the current theme choice to local storage
+    const theme = body.classList.contains("dark") ? "dark" : "light";
+    localStorage.setItem("theme", theme);
+  }
+
+  // Update the last tap timestamp
+  lastTap = now;
 });
