eab4812828bd80fa08ed0616143d31085afa3a97
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-03-26 02:32:51 +0100

    update dc like with lanyard

---
+++ website/Assets/JS/config.js
@@ -0,0 +1,39 @@
+const BADGES = [
+  {
+    name: "HypeSquad Bravery", // Message that will appear when hovering over the badge
+    image: "assets/badges/hypesquad_bravery.svg",
+  },
+  {
+    name: "Subscriber since Jun 29, 2022",
+    image: "assets/badges/nitro.svg",
+  },
+  {
+    name: "Server boosting Jun 29, 2022",
+    image: "assets/badges/boost.svg",
+  },
+  {
+    name: "That's an example! Change in config.js", // Message that appears when you hover over it
+    image: "assets/badges/bug_hunter_level2.svg", // Icon (in .svg) of the badge that is in the "assets/badges" you choose
+  },
+];
+
+const USER_ID = "898937224895270972"; // Your Discord Id Here | You need to join Lanyard Discord for it to work! [https://discord.gg/z2xW3zxYdt]
+const BACKGROUND_IMAGE = "https://i.pinimg.com/originals/5a/80/29/5a8029d622d15914e020276a69578e51.gif"; // Here is the background of the site, behind your profile
+const STATUS = {
+  dnd: {
+    name: "Do not disturb", // Discord Status
+    color: "#ed4245",
+  },
+  idle: {
+    name: "Busy",
+    color: "#faa81a",
+  },
+  online: {
+    name: "Online",
+    color: "#3ba55d",
+  },
+  offline: {
+    name: "Offline",
+    color: "#6e7985",
+  },
+};
