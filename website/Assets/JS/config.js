4f75015b4c8fd594e2989aec76535f61a1232bfc
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-03-27 23:52:55 +0100

    improving it for tanapat

--- website/Assets/JS/config.js
+++ website/Assets/JS/config.js
@@ -3,14 +3,7 @@ const BADGES = [
     name: "HypeSquad Bravery", // Message that will appear when hovering over the badge
     image: "assets/badges/hypesquad_bravery.svg",
   },
-  {
-    name: "Subscriber since Jun 29, 2022",
-    image: "assets/badges/nitro.svg",
-  },
-  {
-    name: "Server boosting Jun 29, 2022",
-    image: "assets/badges/boost.svg",
-  },
+  
  
 ];
 
@@ -18,11 +11,11 @@ const USER_ID = "898937224895270972"; // Your Discord Id Here | You need to join
 const BACKGROUND_IMAGE = "https://i.pinimg.com/originals/5a/80/29/5a8029d622d15914e020276a69578e51.gif"; // Here is the background of the site, behind your profile
 const STATUS = {
   dnd: {
-    name: "Do not disturb", // Discord Status
+    name: "Do not disturb",
     color: "#ed4245",
   },
   idle: {
-    name: "Busy",
+    name: "AFK",
     color: "#faa81a",
   },
   online: {
