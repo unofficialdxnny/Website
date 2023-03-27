f07a3329841b3f31686a46864982f67c26b7747f
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-03-27 23:57:31 +0100

    removed badges code

--- website/Assets/JS/config.js
+++ website/Assets/JS/config.js
@@ -1,14 +1,6 @@
-const BADGES = [
-  {
-    name: "HypeSquad Bravery", // Message that will appear when hovering over the badge
-    image: "assets/badges/hypesquad_bravery.svg",
-  },
-  
- 
-];
 
-const USER_ID = "898937224895270972"; // Your Discord Id Here | You need to join Lanyard Discord for it to work! [https://discord.gg/z2xW3zxYdt]
-const BACKGROUND_IMAGE = "https://i.pinimg.com/originals/5a/80/29/5a8029d622d15914e020276a69578e51.gif"; // Here is the background of the site, behind your profile
+const USER_ID = "898937224895270972";
+const BACKGROUND_IMAGE = "https://i.pinimg.com/originals/5a/80/29/5a8029d622d15914e020276a69578e51.gif";
 const STATUS = {
   dnd: {
     name: "Do not disturb",
