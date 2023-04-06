d538de082170f4283154bac57ee34308c1623f21
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-04-06 01:40:37 +0100

    about me section DONE

--- website/Assets/JS/lanyard.js
+++ website/Assets/JS/lanyard.js
@@ -1,22 +1,35 @@
-async function fetchData() {
-    const res = await fetch('https://api.lanyard.rest/v1/users/898937224895270972');
-    const data = await res.json();
-    return data;
-  }
-  
-  async function renderData() {
-    const data = await fetchData();
-    const profilePic = document.getElementById('profile-pic');
-    const status = document.getElementById('status');
+const userId = "898937224895270972"; 
+const lanyardUrl = `https://api.lanyard.rest/v1/users/${userId}`;
+const profilePicture = document.getElementById("profile-picture");
+const username = document.getElementById("username");
+const profession = document.getElementById("profession");
+const years = document.getElementById("years");
+const expertise = document.getElementById("expertise");
+const hobbies = document.getElementById("hobbies");
+const interests = document.getElementById("interests");
+
+fetch(lanyardUrl)
+  .then(response => response.json())
+  .then(data => {
+    // Set profile picture
+    profilePicture.src = data.data.discord_user.avatar_url;
     
-    profilePic.src = `https://cdn.discordapp.com/avatars/${data.discord_user.id}/${data.discord_user.avatar}.png`;
+    // Set username
+    username.textContent = data.data.discord_user.username;
     
-    if (data.discord_status === 'online') {
-      status.style.backgroundColor = '#43b581';
-    } else {
-      status.style.backgroundColor = '#747f8d';
-    }
-  }
-  
-  renderData();
-  
+    // Set profession (replace with your own information)
+    profession.textContent = "Web Developer";
+    
+    // Set years of experience (replace with your own information)
+    years.textContent = "5";
+    
+    // Set area of expertise (replace with your own information)
+    expertise.textContent = "Front-End Development";
+    
+    // Set hobbies (replace with your own information)
+    hobbies.textContent = "Coding, Gaming, Watching Movies";
+    
+    // Set interests (replace with your own information)
+    interests.textContent = "Technology, Science, Space";
+  })
+  .catch(error => console.error(error));
