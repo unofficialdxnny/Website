d636cd1c4114cfa82740c365f37b671cee1d7db9
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-04-06 18:05:14 +0100

    DONT BE AFK

---
+++ website/Assets/JS/audio.js
@@ -0,0 +1,23 @@
+var audio = document.getElementById("myAudio");
+		var timeout;
+
+		function startTimer() {
+		  timeout = setTimeout(playAudio, 10000); // set timer for 10 seconds
+		}
+
+		function resetTimer() {
+		  clearTimeout(timeout);
+		  startTimer();
+		}
+
+		function playAudio() {
+		  audio.play();
+		}
+
+		// add event listeners to detect user activity
+		document.addEventListener("mousemove", resetTimer);
+		document.addEventListener("keypress", resetTimer);
+		document.addEventListener("click", resetTimer);
+
+		// start the timer on page load
+		startTimer();
