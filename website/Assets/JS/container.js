bc7b2e08446490c5affb3a2b1dc5001eea585ea9
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-04-17 04:31:42 +0100

    deleted useless files

--- website/Assets/JS/container.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('resize', resizeBanner);
-
-function resizeBanner() {
-  const banner = document.querySelector('.banner');
-  const bannerHeight = banner.offsetWidth / 3;
-  banner.style.height = `${bannerHeight}px`;
-}
-
-resizeBanner();
--- website/Assets/JS/loader.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener('load', function() {
-  var preloader = document.querySelector('.preloader');
-  preloader.classList.add('loaded');
-});
