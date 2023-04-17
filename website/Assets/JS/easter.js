514227746de1533f5863f803aeb2d78a63d3ffd8
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-04-17 04:00:41 +0100

    RESET CUZ BORED LOL

--- website/Assets/JS/easter.js
+++
@@ -1,6 +0,0 @@
-const egg = document.querySelector('.easter-egg');
-egg.addEventListener('click', () => {
-  egg.style.animation = 'none';
-  egg.offsetHeight;
-  egg.style.animation = null;
-});
--- website/Assets/JS/scroll-fade.js
+++
@@ -1,8 +0,0 @@
-$(window).on('scroll', function() {
-    $('section').each(function() {
-      if($(this).offset().top < $(window).scrollTop() + $(window).height() && $(this).css('opacity') == 0) {
-        $(this).addClass('animate__fadeIn');
-      }
-    });
-  });
-  
