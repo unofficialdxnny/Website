0a7bd2dd0e463a62ac10f0da356cb1e0106a8fc6
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-05-01 19:11:25 +0100

    splash screen

--- website/Assets/JS/Index.js
+++ website/Assets/JS/Index.js
@@ -2,3 +2,13 @@ setInterval(function() {
     var myImageElement = document.getElementById('Spotify-now-playing');
     myImageElement.src = 'https://spotify-github-profile.vercel.app/api/view?uid=ahmeddanial&cover_image=true&theme=novatorem&show_offline=false&background_color=121212&interchange=false&bar_color=53b14f&bar_color_cover=false' + Math.random();
   }, 3000);
+
+
+// Wait for the image to load and then show the content
+const image = document.querySelector('.splash img');
+image.addEventListener('load', function() {
+  setTimeout(() => {
+    document.querySelector('.splash').classList.add('hide');
+    document.querySelector('.content').classList.add('show');
+  }, 2000); // Change the delay as needed
+});
