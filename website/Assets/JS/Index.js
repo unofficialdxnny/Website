20557c29ff1e9012ccd1e6ac027b2103ae3d0aeb
unofficialdxnny <ahmeddanial324@gmail.com>
2023-07-18 16:16:26 +0100

    stranger things

--- website/Assets/JS/Index.js
+++ website/Assets/JS/Index.js
@@ -9,3 +9,32 @@ setInterval(function() {
     event.preventDefault();
   });
   
+
+  const button = document.getElementById("strangerthings");
+
+// Add a click event listener to the button
+button.addEventListener("click", function() {
+  // Text to be copied to the clipboard
+  const textToCopy = "https://gamepadviewer.com/?p=1&s=8&editcss=https%3A%2F%2Funofficialdxnny.netlify.app%2FAssets%2FCSS%2Fstrangerthings.css";
+
+  // Create a temporary textarea element
+  const textarea = document.createElement("textarea");
+  textarea.value = textToCopy;
+  document.body.appendChild(textarea);
+
+  // Select the text within the textarea
+  textarea.select();
+  textarea.setSelectionRange(0, textarea.value.length);
+
+  // Copy the selected text to the clipboard
+  document.execCommand("copy");
+
+  // Remove the temporary textarea
+  document.body.removeChild(textarea);
+
+  // Optionally, provide visual feedback to the user
+  button.textContent = "Copied!";
+  setTimeout(function() {
+    button.textContent = "Click to Copy";
+  }, 2000);
+});
