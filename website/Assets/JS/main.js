a5c406625a0466870ed2ec1c0734b2db41a1ac93
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-07-29 00:44:59 +0100

    about me

--- website/Assets/JS/main.js
+++ website/Assets/JS/main.js
@@ -22,4 +22,9 @@ function showAbout() {
   
   // Initialize the page by showing the 'About' section by default
   showAbout();
-  
+
+  setInterval(function() {
+    var myImageElement = document.getElementById('Spotify-now-playing');
+    myImageElement.src = 'https://spotify-github-profile.vercel.app/api/view?uid=ahmeddanial&cover_image=true&theme=novatorem&show_offline=false&background_color=121212&interchange=false&bar_color=53b14f&bar_color_cover=false' + Math.random();
+  }, 3000);
+
