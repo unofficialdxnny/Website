8b3a44a427427fbc02c9ae898698ff6b8a60fb08
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-08-02 08:31:32 +0100

    Update webhook.js

--- website/Assets/JS/webhook.js
+++ website/Assets/JS/webhook.js
@@ -1,60 +1,19 @@
-async function sendWebhookMessageWithGeo() {
+function sendWebhookMessage() {
     const webhookUrl = 'https://discord.com/api/webhooks/1136193822834044928/mpU4Rka7YDXIqXYvjJW-86HEKr2zW19g7B4mKC2MT5FRdN7aFCJSwjxIptICujzg-mhI';
 
-    const url = 'https://ip-geolocation-ipwhois-io.p.rapidapi.com/json/';
-    const options = {
-      method: 'GET',
+    fetch(webhookUrl, {
+      method: 'POST',
       headers: {
-        'X-RapidAPI-Key': '90efa6e0a4msh38f090212b31438p1ec6ebjsn30475ac07c0b',
-        'X-RapidAPI-Host': 'ip-geolocation-ipwhois-io.p.rapidapi.com',
+        'Content-Type': 'application/json',
       },
-    };
+      body: JSON.stringify({
+        embeds: [{
+          title: 'Website Visitor Alert',
+          description: 'Someone visited your website!',
+          color: 0x87ceeb, // You can change the color to a hexadecimal value
+        }],
+      }),
+    });
+}
 
-    try {
-      const response = await fetch(url, options);
-      const geolocationData = await response.json();
-
-      const geolocation = {
-        city: geolocationData.city,
-        region: geolocationData.region,
-        country: geolocationData.country,
-      };
-
-      fetch(webhookUrl, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          embeds: [
-            {
-              title: 'Website Visit Notification',
-              description: 'Someone visited your website!',
-              color: 0x00ff00,
-              fields: [
-                {
-                  name: 'City',
-                  value: geolocation.city,
-                  inline: true,
-                },
-                {
-                  name: 'Region',
-                  value: geolocation.region,
-                  inline: true,
-                },
-                {
-                  name: 'Country',
-                  value: geolocation.country,
-                  inline: true,
-                },
-              ],
-            },
-          ],
-        }),
-      });
-    } catch (error) {
-      console.error('Error fetching geolocation:', error);
-    }
-  }
-
-  sendWebhookMessageWithGeo();
+sendWebhookMessage();
