02ef1dc38951a403824451466f9df31cb17095fb
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-08-02 06:59:07 +0100

    buttons

--- website/Assets/JS/main.js
+++ website/Assets/JS/main.js
@@ -31,68 +31,80 @@ const gamepadItems = [
     name: "Galactic",
     date: "01/08/2023",
     url: "https://unofficialdxnny.com/gamepad/galactic",
+    category: "ds3",
   },
   {
     imageUrl: "./Assets/Images/st.png",
     name: "Stranger Things",
     date: "01/03/2021",
     url: "https://unofficialdxnny.com/gamepad/strangerthings",
+    category: "ds4",
   },
   {
     imageUrl: "./Assets/Images/camo.png",
     name: "Camo",
     date: "22/07/23",
     url: "https://unofficialdxnny.com/gamepad/camo",
+    category: "ds4",
   },
   {
     imageUrl: "./Assets/Images/ds4whitee.png",
     name: "DS4 White",
     date: "24/07/23",
     url: "https://unofficialdxnny.com/gamepad/ds4white",
+    category: "ds4",
   },
   {
     imageUrl: "./Assets/Images/mmd.png",
     name: "Miles Morales Dark",
     date: "22/07/2023",
     url: "https://unofficialdxnny.com/gamepad/milesmoralesd",
+    category: "ds5",
   },
   {
     imageUrl: "./Assets/Images/mmw.png",
     name: "Miles Morales Light",
     date: "22/07/2023",
     url: "https://unofficialdxnny.com/gamepad/milesmoralesl",
+    category: "ds5",
   },
   {
     imageUrl: "./Assets/Images/fxb1.png",
     name: "Feather",
     date: "20/07/2023",
     url: "https://unofficialdxnny.com/gamepad/feather",
+    category: "xb1",
   },
 
 ];
 
 // Function to dynamically generate the content for Gamepad items
-function loadGamepadItems() {
+function showCategory(category) {
   const gamepadGallery = document.getElementById("gamepadGallery");
 
   // Clear the existing content
   gamepadGallery.innerHTML = "";
 
-  // Loop through the gamepadItems array and generate HTML for each item
+  // Loop through the gamepadItems array and filter items based on the category
   gamepadItems.forEach((item) => {
-    const itemHtml = `
-      <div class="image">
-        <a target="_blank" href="${item.url}">
-          <img src="${item.imageUrl}" />
-        </a>
-        <p style="text-align: center; font-size: 10px;">${item.name} (${item.date})</p>
-      </div>
-    `;
+    if (category === "all" || item.category === category) {
+      const itemHtml = `
+        <div class="image">
+          <a target="_blank" href="#">
+            <img src="${item.imageUrl}" />
+          </a>
+          <p style="text-align: center; font-size: 10px;">${item.name} (${item.date})</p>
+        </div>
+      `;
 
-    // Append the itemHtml to the gallery container
-    gamepadGallery.innerHTML += itemHtml;
+      // Append the itemHtml to the gallery container
+      gamepadGallery.innerHTML += itemHtml;
+    }
   });
 }
+
+showCategory("all");
+
 
 setInterval(function () {
   var myImageElement = document.getElementById("Spotify-now-playing");
