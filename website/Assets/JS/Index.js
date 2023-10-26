734301f9ac3428b0b4b124a1067b8ea10f6ed9e0
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-10-26 02:57:49 +0100

    project cards

--- website/Assets/JS/Index.js
+++ website/Assets/JS/Index.js
@@ -4,3 +4,15 @@ setInterval(function () {
       "https://spotify-github-profile.vercel.app/api/view?uid=ahmeddanial&cover_image=true&theme=novatorem&show_offline=false&background_color=121212&interchange=false&bar_color=53b14f&bar_color_cover=false" +
       Math.random();
   }, 3000);
+
+
+
+
+
+
+
+
+
+// Projects
+
+
