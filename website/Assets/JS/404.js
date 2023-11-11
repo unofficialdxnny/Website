2ce2cc2b4a391eed80799449751df76555d5a9a4
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-11-10 19:20:19 -0800

    navbar

--- website/Assets/JS/404.js
+++
@@ -1,6 +0,0 @@
-// This code is optional and can be used to redirect the user to the homepage after a certain amount of time
-
-setTimeout(function() {
-    window.location.href = "https://unofficialdxnny.netlify.app/";
-  }, 5000);
-  
