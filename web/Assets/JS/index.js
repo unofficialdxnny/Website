0e91dc58373a43b858ce066842721251a8a808aa
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2024-05-09 00:43:43 +0100

    added status bar colour to snp

--- web/Assets/JS/index.js
+++ web/Assets/JS/index.js
@@ -1,44 +1,48 @@
-// online status
 async function updateDiscordStatus() {
-    try {
-      const response = await fetch('https://api.lanyard.rest/v1/users/898937224895270972');
-      const data = await response.json();
-  
-      if (response.ok) {
-        const status = data.data.discord_status;
-        const h1Element = document.getElementById('discord-status');
-        const mainElement = document.querySelector('.main');
-  
-        // Change color based on status
-        switch (status) {
-          case 'online':
-            h1Element.style.color = 'green';
-            mainElement.style.backgroundColor = 'green';
-            break;
-          case 'idle':
-            h1Element.style.color = 'yellow';
-            mainElement.style.backgroundColor = 'yellow';
-            break;
-          case 'dnd':
-            h1Element.style.color = 'red';
-            mainElement.style.backgroundColor = 'red';
-            break;
-          case 'offline':
-            h1Element.style.color = 'gray';
-            mainElement.style.backgroundColor = 'gray';
-            break;
-          default:
-            h1Element.style.color = 'black';
-            mainElement.style.backgroundColor = 'white';
-        }
-      } else {
-        console.error('Error fetching Discord status:', data);
+  try {
+    const response = await fetch('https://api.lanyard.rest/v1/users/898937224895270972');
+    const data = await response.json();
+
+    if (response.ok) {
+      const status = data.data.discord_status;
+      const h1Element = document.getElementById('discord-status');
+      const mainElement = document.querySelector('.main');
+      const imageElement = document.querySelector('.snp img');
+
+      // Change color based on status
+      switch (status) {
+        case 'online':
+          h1Element.style.color = 'green';
+          mainElement.style.backgroundColor = 'green';
+          imageElement.src = imageElement.src.replace(/bar_color=.*?&/, 'bar_color=4f8832&');
+          break;
+        case 'idle':
+          h1Element.style.color = 'yellow';
+          mainElement.style.backgroundColor = 'yellow';
+          imageElement.src = imageElement.src.replace(/bar_color=.*?&/, 'bar_color=f79c18&');
+          break;
+        case 'dnd':
+          h1Element.style.color = 'red';
+          mainElement.style.backgroundColor = 'red';
+          imageElement.src = imageElement.src.replace(/bar_color=.*?&/, 'bar_color=812e25&');
+          break;
+        case 'offline':
+          h1Element.style.color = 'gray';
+          mainElement.style.backgroundColor = 'gray';
+          imageElement.src = imageElement.src.replace(/bar_color=.*?&/, 'bar_color=gray&');
+          break;
+        default:
+          h1Element.style.color = 'black';
+          mainElement.style.backgroundColor = 'white';
+          imageElement.src = imageElement.src.replace(/bar_color=.*?&/, 'bar_color=000000&');
       }
-    } catch (error) {
-      console.error('Error fetching Discord status:', error);
+    } else {
+      console.error('Error fetching Discord status:', data);
     }
+  } catch (error) {
+    console.error('Error fetching Discord status:', error);
   }
-  
-  
-  updateDiscordStatus();
-  setInterval(updateDiscordStatus, 1000);
+}
+
+updateDiscordStatus();
+setInterval(updateDiscordStatus, 1000);
