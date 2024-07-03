7c4574a3082eb099f6c1334507b138d55c0a77c1
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2024-07-03 02:09:17 +0100

    toggle theme

--- unofficialdxnny/Assets/js/index.js
+++ unofficialdxnny/Assets/js/index.js
@@ -1,5 +1,18 @@
-// page fade in on load
+// theme
 
-document.addEventListener('DOMContentLoaded', function () {
-    document.body.classList.add('fadeIn');
+const themeToggle = document.getElementById('toggle-button');
+
+themeToggle.addEventListener('click', function () {
+    const body = document.body;
+    body.classList.toggle('dark');
+    const sunIcon = document.querySelector('.sun-icon');
+    const moonIcon = document.querySelector('.moon-icon');
+
+    if (body.classList.contains('dark')) {
+        sunIcon.style.display = 'none';
+        moonIcon.style.display = 'inline-block';
+    } else {
+        moonIcon.style.display = 'none';
+        sunIcon.style.display = 'inline-block';
+    }
 });
